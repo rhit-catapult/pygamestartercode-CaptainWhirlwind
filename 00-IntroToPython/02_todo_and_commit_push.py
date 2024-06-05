6494c80a5c79cf56426ef8cf89d98920a4dd6a60
CaptainWhirlwind <finnpalazzari@gmail.com>
2024-06-05 15:25:59 -0400

    done

--- 00-IntroToPython/02_todo_and_commit_push.py
+++ 00-IntroToPython/02_todo_and_commit_push.py
@@ -12,10 +12,10 @@
 #   Anything surrounded by quote marks (single or double) is a STRING.
 #
 ###############################################################################
-print("Hello, World")
+
 
 ###############################################################################
-# TODO: 1.
+# DONE: 1
 #   (Yes, that means for YOU to DO things per these instructions:)
 #  _
 #   Run this module by right clicking anywhere in this window
@@ -29,7 +29,7 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing "to do" in this module.
 #  _
@@ -52,7 +52,7 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below this comment.
 #   It should print any string that you want (but keep it polite and G-rated!)
 #  _
@@ -64,7 +64,7 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).

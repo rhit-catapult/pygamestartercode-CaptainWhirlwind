af204469c31d9103fe02dbcd50406d912292ad29
CaptainWhirlwind <finnpalazzari@gmail.com>
2024-06-05 16:59:14 -0400

    Added nose

--- 00-IntroToPython/02_todo_and_commit_push.py
+++ 00-IntroToPython/02_todo_and_commit_push.py
@@ -64,7 +64,7 @@
 ###############################################################################
 
 ###############################################################################
-# DONE: 4.
+# TODO: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).

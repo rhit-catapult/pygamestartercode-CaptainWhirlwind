ee4bc0ff496182391b81dd6f03aacdf45f936c38
CaptainWhirlwind <finnpalazzari@gmail.com>
2024-06-07 15:09:13 -0400

    Done

--- 05-BouncingBall/BouncingBall.py
+++ 05-BouncingBall/BouncingBall.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import math
 
 
 # You will implement this module ENTIRELY ON YOUR OWN!
@@ -8,14 +9,56 @@ import random
 # TODO: Create a Ball class.
 # TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
 # TODO: Methods: __init__, draw, move
+class Ball:
+    def __init__(self, screen, color, x, y, radius, direction, speed):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.direction = direction
+        self.color = color
+        self.xspeed = math.cos(self.direction) * speed
+        self.yspeed = math.sin(self.direction) * speed
 
+    def move(self):
+        self.x += self.xspeed
+        self.y += self.yspeed
 
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+    def vector(self):
+        if self.y <= self.radius:
+            self.yspeed = 0-self.yspeed
+        elif self.y >= 600-self.radius:
+            self.yspeed = 0-self.yspeed
+        elif self.x < self.radius:
+            self.xspeed = 0-self.xspeed
+        elif self.x > 600-self.radius:
+            self.xspeed = 0-self.xspeed
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption('Bouncing Ball')
-    screen.fill(pygame.Color('gray'))
+    screen.fill(pygame.Color('white'))
     clock = pygame.time.Clock()
+
+
+    balls = []
+    for k in range(10):
+        x = random.randint(150, 450)
+        y = random.randint(150, 450)
+        radius = random.randint(10, 50)
+        direction = random.randint(0, 360)
+        color1 = random.randint(0, 255)
+        color2 = random.randint(0, 255)
+        color3 = random.randint(0, 255)
+        speed = random.randint(3,15)
+
+        color = (color1, color2, color3)
+
+        ball = Ball(screen, color, x, y, radius, direction, speed)
+        balls.append(ball)
 
     # TODO: Create an instance of the Ball class called ball1
 
@@ -23,9 +66,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
         clock.tick(60)
-        screen.fill(pygame.Color('gray'))
+        screen.fill(pygame.Color('white'))
+
+        for ball in balls:
+            ball.vector()
+            ball.move()
+            ball.draw()
 
         # TODO: Move the ball
         # TODO: Draw the ball
